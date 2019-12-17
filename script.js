14f497ed5a15ca092e7c2b14c83b0068582314ea
DR-84 <58080204+DR-84@users.noreply.github.com>
2019-12-17 10:23:49 +0100

    opdracht 1 w3d2

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('He hallo, ik ben eerste');
+setTimeout(function() {
+	console.log('He hallo, ik ben tweede');
+}, 2000);
+console.log('He hallo, ik ben derde');
+
+/* document.addEventListener('DOMContentLoaded', event => {
+	console.log('DOM fully loaded and parsed');
+});
+ */

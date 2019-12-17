b662262f8683aaa5f1e62520ad74e6ac8bbca0ec
DR-84 <58080204+DR-84@users.noreply.github.com>
2019-12-17 11:10:00 +0100

    opdracht 2 w3d2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-console.log('He hallo, ik ben eerste');
-setTimeout(function() {
-	console.log('He hallo, ik ben tweede');
-}, 2000);
-console.log('He hallo, ik ben derde');
+const huiswerkMaken = function(vak, callback) {
+	console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+	callback();
+};
+
+const klaarMetHuiswerk = function() {
+	setTimeout(() => {
+		console.log(`Kijk Paps/Mams, ik ben klaar met mijn huiswerk!`);
+	}, 5000);
+};
+
+huiswerkMaken('wiskunde', klaarMetHuiswerk);
+//
+//
 
 /* document.addEventListener('DOMContentLoaded', event => {
 	console.log('DOM fully loaded and parsed');
